2b606d5f2ca39a96576dcf2032425e032f374806
lucafo13 <odavilucas587@gmail.com>
2026-06-05 18:45:01 -0300

    ajustes | vite

---
+++ JS/api.js
@@ -0,0 +1,18 @@
+const axios = require("axios")
+
+
+const API = async () => {
+    try{
+    const { data } = await axios.get("https://api.exchangerate.host/latest", {
+        params:{
+            base: "USD",
+            symbols: "BRL"
+        }
+    })
+    console.log(data)
+}
+    catch(error){
+        console.log("pau no axios", { error })
+    }   
+}
+API()
