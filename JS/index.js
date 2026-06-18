0d3183fae7d8d87d22ed75df36fb72d47d9b5cb0
lucafo13 <odavilucas587@gmail.com>
2026-06-18 09:37:39 -0300

    ajuste || update

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,10 @@
 emailjs.init("5_JqmtV7HfHYu5zbd");
+
+
+const login = document.querySelector('#login');
+
+login.addEventListener('click', () => {
+    settimeout(() => {
+        window.location.href = 'login.html';
+    }, 1000);
+})
