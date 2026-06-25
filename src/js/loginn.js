ad3d33b253adcee1ffe04355d1dd3e18f386cc01
lucafo13 <odavilucas587@gmail.com>
2026-06-25 09:31:00 -0300

    ajuste || update

--- src/js/loginn.js
+++ src/js/loginn.js
@@ -40,8 +40,6 @@ emailjs.init("5_JqmtV7HfHYu5zbd");
     } 
     const cadog = document.getElementById("log-btnx")
     cadog.addEventListener('click', logar)
-passForget.addEventListener("click", () => {
-    window.location.href = "/passForget.html"
-})
+
     const logg = document.getElementById('logg')
     logg.addEventListener('click', () => {window.location.href = '/login.html'})
