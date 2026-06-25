9b5d41c6f53ec1f00dd772ceede9adfd045b7f5a
lucafo13 <odavilucas587@gmail.com>
2026-06-25 08:50:30 -0300

    e ainda c vieeeer noites traiçoeiraas pt2s

--- JS/index.js
+++ JS/index.js
@@ -14,13 +14,5 @@ if(!nome){
 document.getElementById('name').textContent = nome
 }
 
-const irAnalises = document.getElementById('analises')
-irAnalises.addEventListener('click', () => {
-    if(!localStorage.getItem('nome')){
-        alert('Faça Login primeiro')
-    } 
-    window.location.href = '/analises.html'
-    
-})
 // effect
 
