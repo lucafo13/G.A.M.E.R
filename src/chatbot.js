935b036a83bec56caa96a28ffe69f3877784bdc1
Luan Vicktor <luanvicktor7@gmail.com>
2026-06-29 02:26:41 -0300

    detalhe icon

--- src/chatbot.js
+++ src/chatbot.js
@@ -1,6 +1,5 @@
 const apiKey = import.meta.env.VITE_TECHNOBOTKEY;
 import axios from "axios";
-//personalidade dele, vou add mais coisa
 const contextoEmpresa = `
     
 Você é o TechnoBot, o cérebro digital e assistente virtual oficial da International G.A.M.E.R. 
